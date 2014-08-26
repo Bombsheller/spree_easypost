8f8c112263409eba4d60b6f7aab24d495b07e13f
Lucas S Eggers <l.eggers91@gmail.com>
2014-08-26 18:08:26 -0300

    only show print label link if tracking details are present

--- app/overrides/add_shipping_label_button_to_admin_edit_order.rb
+++ app/overrides/add_shipping_label_button_to_admin_edit_order.rb
@@ -2,7 +2,7 @@ Deface::Override.new(
   :name => "proper_display_of_order_shipments",
   :virtual_path => 'spree/admin/orders/_shipment',
   :insert_bottom => 'tr.show-tracking > td[colspan="5"]',
-  :text => "<% if shipment.selected_shipping_rate.easy_post_rate_id %>
+  :text => "<% if shipment.tracking.present? %>
               <%= link_to 'Get Shipping Label', shipping_label_admin_shipment_url(shipment.id), target: '_blank' %>
             <% end %>"
   )
